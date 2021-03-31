88ea5b476d2b5469e88f5864ef5486027f4a0e78
Jianchao Li <li.jianc@northeastern.edu>
2021-03-31 19:37:34 -0400

    update

--- Final Project/src/main/scala/ProcessData.scala
+++ Final Project/src/main/scala/ProcessData.scala
@@ -1,16 +1,16 @@
 import org.apache.spark.sql.SparkSession
 
-object ProcessData extends App{
+object ProcessData extends App {
   val spark: SparkSession = SparkSession
     .builder()
     .appName("ProcessData")
     .master("local[*]")
     .getOrCreate()
 
-  val kills= spark.read.format("csv")
+  val kills = spark.read.format("csv")
     .option("header", "true")
     .option("inferSchema", "true")
-    .load("src\\main\\resources\\kills.csv")
+    .load("src/main/resources/kills.csv")
   kills.printSchema()
   kills.show()
 }
