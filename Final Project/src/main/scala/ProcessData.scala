d0e888b75c419e3c71bdd1083da0eaae861db7b8
Linzzz <zhu.lin3@northeastern.edu>
2021-03-31 19:22:19 -0400

    ProcessData update1

--- Final Project/src/main/scala/ProcessData.scala
+++ Final Project/src/main/scala/ProcessData.scala
@@ -10,7 +10,21 @@ object ProcessData extends App{
   val kills= spark.read.format("csv")
     .option("header", "true")
     .option("inferSchema", "true")
-    .load("src\\main\\resources\\kills.csv")
+    .load("src/main/resources/kills.csv")
   kills.printSchema()
   kills.show()
+
+  val bans= spark.read.format("csv")
+    .option("header", "true")
+    .option("inferSchema", "true")
+    .load("src/main/resources/bans.csv")
+  bans.printSchema()
+  bans.show()
+
+  val monsters= spark.read.format("csv")
+    .option("header", "true")
+    .option("inferSchema", "true")
+    .load("src/main/resources/monsters.csv")
+  monsters.printSchema()
+  monsters.show()
 }
