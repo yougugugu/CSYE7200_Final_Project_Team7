5280cd817f8afda6ec93097dc47607fa557dca73
Jianchao Li <li.jianc@northeastern.edu>
2021-04-16 22:03:17 -0400

    mllib config

--- Final Project/src/main/scala/ModelSelect.scala
+++ Final Project/src/main/scala/ModelSelect.scala
@@ -1,4 +1,10 @@
+import org.apache.spark.ml.feature.StringIndexer
 
 object ModelSelect extends App {
   val predict = ProcessData.predData
+
+  val labelIndexer = new StringIndexer()
+    .setInputCol("label")
+    .setOutputCol("indexedLabel")
+    .fit(predict)
 }
